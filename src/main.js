3bebdada8ce3eb084c188982ddfc8a8bb0d6f944
Bharat Arimilli <bbarimilli@outlook.com>
2016-02-02 10:34:32 -0600

    Switch to CDN hosted bootstrap.min.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import AppContainer from './containers/AppContainer';
 import configureStore from './store/configureStore';
 import 'isomorphic-fetch';
 import 'styles/app.scss';
-import 'vendor/js/bootstrap.min.js';
 
 const store = configureStore();
 
