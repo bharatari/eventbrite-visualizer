9ea8b0b3eafffd1a4e51b239d75fde331004dbdb
Bharat Arimilli <bbarimilli@outlook.com>
2016-02-01 21:13:28 -0600

    Remove vendor css imports from main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,7 @@ import { render } from 'react-dom';
 import AppContainer from './containers/AppContainer';
 import configureStore from './store/configureStore';
 import 'isomorphic-fetch';
-import 'vendor/css/bootstrap.min.css';
-import 'vendor/css/ionicons.min.css';
-import 'vendor/css/react-select.min.css';
-import 'styles/app.css';
+import 'styles/app.scss';
 import 'vendor/js/bootstrap.min.js';
 
 const store = configureStore();
